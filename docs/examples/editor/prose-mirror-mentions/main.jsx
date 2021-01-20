5b7bbc6a9226ebfd0572f0b0a7eca7675577d41b
Ina Glushkova <Ina.Glushkova@progress.com>
2021-01-20 17:29:29 +0200

    docs(editor): fix import

--- docs/examples/editor/prose-mirror-mentions/main.jsx
+++ docs/examples/editor/prose-mirror-mentions/main.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { Editor, ProseMirror, EditorUtils } from '../src/main';
+import { Editor, ProseMirror, EditorUtils } from '@progress/kendo-react-editor';
 import { addMentionNodes, addTagNodes, getMentionsPlugin } from 'prosemirror-mentions';
 
 const { Schema, EditorView, EditorState } = ProseMirror;
